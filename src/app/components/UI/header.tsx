90ca879557384500660cad3c01e0601bee4fd895
Mikhail <041411032425mk@gmail.com>
2026-03-11 23:48:18 +0400

    feat: update header and logo feat: add navigation items to header

--- src/app/components/UI/header.tsx
+++ src/app/components/UI/header.tsx
@@ -4,48 +4,50 @@ import {
     NavbarBrand,
     NavbarContent,
     NavbarItem,
-    Link,
     Button,
 } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
 
-export const AcmeLogo = () => {
 
+export const Logo = () => {
     return (
-        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-            <path
-                clipRule="evenodd"
-                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                fill="currentColor"
-                fillRule="evenodd"
-                />
-        </svg>
+        <Image 
+        src="/logo_ruskuhn.png" 
+        alt="Russian cuisine"
+        width={45}
+        height={45}
+        sizes="(max-width: 768px) 100vw, 45px, (max-width: 1200px) 50vw, 45px"
+        priority
+        />
     );
 };
 
 export default function Header({children}: {children: React.ReactNode}) {
+
+    const navItems = [
+        {href: "/", label: "About"},
+        {href: "/Recipes", label: "Recipes"},
+        {href: "/contact", label: "Contact"},
+        {href: "/ingredients", label: "Ingredients"},
+    ]
     return (
         <>
             <Navbar shouldHideOnScroll>
                 <NavbarBrand>
-                <AcmeLogo />
-                <p className="font-bold text-inherit">ACME</p>
+                    <Logo />
+                    <Link href="/"><p className="font-gap-1">RusKuhn</p></Link>
                 </NavbarBrand>
                 <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                <Link color="foreground" href="#">
-                    Features
-                </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link aria-current="page" href="#">
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Integrations
-                    </Link>
-                </NavbarItem>
+                    {navItems.map((item) => {
+                        return(
+                            <NavbarItem key={item.href}>
+                                <Link color="foreground" href={item.href}>
+                                    {item.label}
+                                </Link>
+                            </NavbarItem>
+                        );
+                    })}
                 </NavbarContent>
                 <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
